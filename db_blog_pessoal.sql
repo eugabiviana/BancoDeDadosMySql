32bc5d7abb6b05cf1ababc3a20d47346c37dc28b
unknown <gabi.viana88@gmail.com>
2020-10-03 21:40:29 -0300

    Bando de dados do exercicio blog pessoal

---
+++ db_blog_pessoal.sql
@@ -0,0 +1,8 @@
+create database blog_pessoal;
+
+use blog_pessoal;
+
+insert into `blog_pessoal`.`postagem`(`data`,`texto`,`titulo`)
+values ('2020-04-30 10:10:10.000','Estou aprendendo','API Rest Spring');
+
+select * from postagem;
